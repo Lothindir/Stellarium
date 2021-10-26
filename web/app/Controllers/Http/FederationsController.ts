71869d2423b0acabc3bec5937dc804358e2cc7dd
Lothindir <lothindir@gmail.com>
2021-10-26 18:19:01 +0200

    feat: Added Players routes

--- web/app/Controllers/Http/FederationsController.ts
+++ web/app/Controllers/Http/FederationsController.ts
@@ -1,4 +1,3 @@
-// @ts-nocheck
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import neo4j from 'neo4j-driver';
 import neo4jDriver from '@ioc:Adonis/Addons/Neo4j';
@@ -7,9 +6,12 @@ import Federation from '../../Models/Federation';
 export default class FederationsController {
   public async index({ response }: HttpContextContract) {
     let session = neo4jDriver.session({ defaultAccessMode: neo4j.session.READ });
-    const result = await session.readTransaction((txc) => txc.run('MATCH (f:Federation) RETURN f.name as name'));
+    const result = await session.readTransaction((txc) =>
+      txc.run('MATCH (f:Federation) RETURN f.name as name')
+    );
 
-    const records = result.records.map(r => {
+    console.log(result);
+    const records = result.records.map((r) => {
       return r.get('name');
     });
     response.json(records);
--- web/app/Controllers/Http/PlayersController.ts
+++ web/app/Controllers/Http/PlayersController.ts
@@ -1,7 +1,44 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import neo4j from 'neo4j-driver';
+import neo4jDriver from '@ioc:Adonis/Addons/Neo4j';
 
 export default class PlayersController {
-  public async index({ response }: HttpContextContract) {}
+  public async index({ response }: HttpContextContract) {
+    let session = neo4jDriver.session({ defaultAccessMode: neo4j.session.READ });
+    const result = await session.readTransaction((txc) =>
+      txc.run(
+        'MATCH (p:Player)-[PART_OF]->(c:Crew)-[BELONGS_TO]->(f:Federation) RETURN p.uuid as uuid, p.username as username, c.name as crew, f.name as federation'
+      )
+    );
+    const records = result.records.map((r) => {
+      return {
+        uuid: r.get('uuid'),
+        username: r.get('username'),
+        crew: r.get('crew'),
+        federation: r.get('federation'),
+      };
+    });
+    response.status(200).json(records);
+  }
 
-  public async show({ request, response }: HttpContextContract) {}
+  public async show({ request, response }: HttpContextContract) {
+    const uuid = request.param('id');
+    let session = neo4jDriver.session({ defaultAccessMode: neo4j.session.READ });
+    const result = await session.readTransaction((txc) =>
+      txc.run(
+        'MATCH (p:Player)-[PART_OF]->(c:Crew)-[BELONGS_TO]->(f:Federation) WHERE p.uuid = $uuid RETURN p.uuid as uuid, p.username as username, c.name as crew, f.name as federation',
+        { uuid: uuid }
+      )
+    );
+    const records = result.records.map((r) => {
+      return {
+        uuid: r.get('uuid'),
+        username: r.get('username'),
+        crew: r.get('crew'),
+        federation: r.get('federation'),
+      };
+    });
+    if (records.length !== 0) response.status(200).json(records);
+    else response.status(404).json({ message: 'Player not found', uuid: uuid });
+  }
 }
