95e001b7f6bb8512208ad6ab100342c117f78e29
ghiringh <ghiringhelli.pierreadrien@gmailcom>
2021-10-31 17:59:33 +0100

    added link to rules to default header

--- frontend/components/Header.vue
+++ frontend/components/Header.vue
@@ -21,6 +21,7 @@
             class="menu-element" 
             to="/profil">Profil
             </nuxt-link>
+            <div class="menu-element"><a href="https://asvd.space/les-regles/">Règles</a></div>
             <div class="menu-element" @click="logout">Déconnexion</div>
         </div>
     </header>
