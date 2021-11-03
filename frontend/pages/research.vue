937cc1c8205f3eeb1e1732fca05e2b6679f32697
Tiralka <tiralka@gmail.com>
2021-11-03 19:18:31 +0100

    Research dsplaying from API and Fake

--- frontend/pages/research.vue
+++ frontend/pages/research.vue
@@ -9,27 +9,27 @@
       <div class="level" id="level1">I</div>
       <div class="level" id="level0">Σ</div>
       <!--<button v-for="(planet, index) in crewInfo" :key="index">test</button> -->
-      <button id=CC0 class="validated" @click="showChallengeDescription('CC0')">Centre culturel</button>
-      <button id=ca1 class="validated">Beaux-arts</button>
-      <button id=ca2 class="validated">Esthétique</button>
-      <button id=ca3 class="validable">Génie artistique</button>
-      <button id=ca4 class="inaccessible">Liberté de penser</button>
-      <button id=cb1 class="validated">Age des héros</button>
-      <button id=cb2 class="validated">Age d'airain</button>
-      <button id=cb3 class="validated">Age d'argent</button>
-      <button id=cb4 class="validable">Age d'or</button>
-      <button id=cb5 class="inaccessible">Age du turgonium</button>
-      <button id=cc1 class="validated">Historiographie</button>
-      <button id=cc2 class="validated">Ethnographie</button>
-      <button id=cc3 class="validated">Archéologie</button>
-      <button id=cc4 class="validable">Echange culturel</button>
-      <button id=cc5a class="inaccessible">Humanisme</button>
-      <button id=cc5b class="inaccessible">Révolution culturelle</button>
-      <button id=cd1 class="validated">Transcience</button>
-      <button id=cd2 class="validable">Réforme</button>
-      <button id=cd3 class="inaccessible">Nouveauté</button>
-      <button id=cd4 class="inaccessible">Machinerie</button>
-      <button id=cd5 class="inaccessible">Transhumanisme</button>
+      <button id=CC0 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC0'))" @click="showChallengeDescription('CC0')">Centre culturel</button>
+      <button id=ca1 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC1'))" @click="showChallengeDescription('CC1')">Beaux-arts</button>
+      <button id=ca2 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC5'))" @click="showChallengeDescription('CC5')">Esthétique</button>
+      <button id=ca3 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC9'))" @click="showChallengeDescription('CC9')">Génie artistique</button>
+      <button id=ca4 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC13'))" @click="showChallengeDescription('CC13')">Liberté de penser</button>
+      <button id=cb1 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC2'))" @click="showChallengeDescription('CC2')">Age des héros</button>
+      <button id=cb2 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC6'))" @click="showChallengeDescription('CC6')">Age d'airain</button>
+      <button id=cb3 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC10'))" @click="showChallengeDescription('CC10')">Age d'argent</button>
+      <button id=cb4 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC14'))" @click="showChallengeDescription('CC14')">Age d'or</button>
+      <button id=cb5 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC17'))" @click="showChallengeDescription('CC17')">Age du turgonium</button>
+      <button id=cc1 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC3'))" @click="showChallengeDescription('CC3')">Historiographie</button>
+      <button id=cc2 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC7'))" @click="showChallengeDescription('CC7')">Ethnographie</button>
+      <button id=cc3 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC11'))" @click="showChallengeDescription('CC11')">Archéologie</button>
+      <button id=cc4 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC15'))" @click="showChallengeDescription('CC15')">Echange culturel</button>
+      <button id=cc5a v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC18'))" @click="showChallengeDescription('CC18')">Humanisme</button>
+      <button id=cc5b v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC19'))" @click="showChallengeDescription('CC19')">Révolution culturelle</button>
+      <button id=cd1 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC4'))" @click="showChallengeDescription('CC4')">Transcience</button>
+      <button id=cd2 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC8'))" @click="showChallengeDescription('CC8')">Réforme</button>
+      <button id=cd3 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC12'))" @click="showChallengeDescription('CC12')">Nouveauté</button>
+      <button id=cd4 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC16'))" @click="showChallengeDescription('CC16')">Machinerie</button>
+      <button id=cd5 v-bind:class="getClassFromChallengeStatus(cultureChallenges.find(x => x.id === 'CC20'))" @click="showChallengeDescription('CC20')">Diversité</button>
     </section>
     <section id="militaryContent" class="researchContent hidden">
       <h1>ingénierie militaire</h1>
@@ -38,15 +38,15 @@
       <div class="level" id="level2">II</div>
       <div class="level" id="level1">I</div>
       <div class="level" id="level0">Σ</div>
-      <button id=m0 class="validated">Armement<p>attaque 2<br>defense 2</p></button>
-      <button id=ma1 class="validated">Canons lasers<p>attaque 3</p></button>
-      <button id=ma2 class="validable">Puissance laser I<p>attaque 5</p></button>
-      <button id=ma3 class="inaccessible">Puissance laser II<p>attaque 7</p></button>
-      <button id=ma4 class="inaccessible">Puissance laser III<p>attaque 9</p></button>
-      <button id=mb1 class="validable">Boucliers énergetiques<p>defense 3</p></button>
-      <button id=mb2 class="inaccessible">Energie bleue<p>defense 5</p></button>
-      <button id=mb3 class="inaccessible">Energie rouge<p>defense 7</p></button>
-      <button id=mb4 class="inaccessible">Energie violette<p>defense 9</p></button>
+      <button id=CM0 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM0'))" @click="showChallengeDescription('CM0')">Armement<p>attaque 2<br>defense 2</p></button>
+      <button id=CM1 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM1'))" @click="showChallengeDescription('CM1')">Canons lasers<p>attaque 3</p></button>
+      <button id=CM3 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM3'))" @click="showChallengeDescription('CM3')">Puissance laser I<p>attaque 5</p></button>
+      <button id=CM5 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM5'))" @click="showChallengeDescription('CM5')">Puissance laser II<p>attaque 7</p></button>
+      <button id=CM7 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM7'))" @click="showChallengeDescription('CM7')">Puissance laser III<p>attaque 9</p></button>
+      <button id=CM2 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM2'))" @click="showChallengeDescription('CM2')">Boucliers énergetiques<p>defense 3</p></button>
+      <button id=CM4 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM4'))" @click="showChallengeDescription('CM4')">Energie bleue<p>defense 5</p></button>
+      <button id=CM6 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM6'))" @click="showChallengeDescription('CM6')">Energie rouge<p>defense 7</p></button>
+      <button id=CM8 v-bind:class="getClassFromChallengeStatus(militaryChallenges.find(x => x.id === 'CM8'))" @click="showChallengeDescription('CM8')">Energie violette<p>defense 9</p></button>
     </section>
     <section id="explorationContent" class="researchContent hidden">
       <h1>Exploration</h1>
@@ -55,15 +55,15 @@
       <div class="level" id="level2">II</div>
       <div class="level" id="level1">I</div>
       <div class="level" id="level0">Σ</div>
-      <button id=e0 class="validated">Aménagement<p>carburant max 10<br>recharge 10/j</p></button>
-      <button id=ea1 class="validated">Bidon rouillé<p>carburant max 25</p></button>
-      <button id=ea2 class="validable">Baignoire adaptée<p>carburant max 35</p></button>
-      <button id=ea3 class="inaccessible">Compresseur<p>carburant max 50</p></button>
-      <button id=ea4 class="inaccessible">Carburant quantique<p>carburant max 75</p></button>
-      <button id=eb1 class="validable">Alambique<p>recharge 25/j</p></button>
-      <button id=eb2 class="inaccessible">Recycleur<p>recharge 35/j</p></button>
-      <button id=eb3 class="inaccessible">Catalyseur<p>recharge 50/j</p></button>
-      <button id=eb4 class="inaccessible">Accélérateur de particules<p>recharge 75/j</p></button>
+      <button id=CE0 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE0'))" @click="showChallengeDescription('CE0')">Aménagement<p>carburant max 10<br>recharge 10/j</p></button>
+      <button id=CE1 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE1'))" @click="showChallengeDescription('CE1')">Bidon rouillé<p>carburant max 25</p></button>
+      <button id=CE3 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE3'))" @click="showChallengeDescription('CE3')">Baignoire adaptée<p>carburant max 35</p></button>
+      <button id=CE5 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE5'))" @click="showChallengeDescription('CE5')">Compresseur<p>carburant max 50</p></button>
+      <button id=CE7 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE7'))" @click="showChallengeDescription('CE7')">Carburant quantique<p>carburant max 75</p></button>
+      <button id=CE2 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE2'))" @click="showChallengeDescription('CE2')">Alambique<p>recharge 25/j</p></button>
+      <button id=CE4 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE4'))" @click="showChallengeDescription('CE4')">Recycleur<p>recharge 35/j</p></button>
+      <button id=CE6 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE6'))" @click="showChallengeDescription('CE6')">Catalyseur<p>recharge 50/j</p></button>
+      <button id=CE8 v-bind:class="getClassFromChallengeStatus(explorationChallenges.find(x => x.id === 'CE8'))" @click="showChallengeDescription('CE8')">Accélérateur de particules<p>recharge 75/j</p></button>
     </section>
     <section id="productionContent" class="researchContent hidden">
       <h1>Production</h1>
@@ -73,27 +73,27 @@
       <div class="level" id="level2">II</div>
       <div class="level" id="level1">I</div>
       <div class="level" id="level0">Σ</div>
-      <button id=p0 class="validated">Usine</button>
-      <button id=pa1 class="validated">Source de H2O<p>ISP + 3h</p></button>
-      <button id=pa2 class="validated">Bisse sans fin<p>ISP + 3h</p></button>
-      <button id=pa3 class="validable">Puit stellaire<p>ISP + 3h</p></button>
-      <button id=pa4 class="inaccessible">Pompe à énéergie solaire<p>ISP + 3h</p></button>
-      <button id=pa5 class="inaccessible">Barrage de l'Hongrin<p>ISP + 3h</p></button>
-      <button id=pb1 class="validated">Pile nucléaire<p>ISP + 3h</p></button>
-      <button id=pb2 class="validated">Batterie lunaire<p>ISP + 3h</p></button>
-      <button id=pb3 class="validated">Batterie solaire<p>ISP + 3h</p></button>
-      <button id=pb4 class="validable">Réservoir électrique<p>ISP + 3h</p></button>
-      <button id=pb5 class="inaccessible">Réservoir sans fond<p>ISP + 3h</p></button>
-      <button id=pc1 class="validated">Charrue à air comprimé<p>ISP + 3h</p></button>
-      <button id=pc2 class="validated">Terreau stellaire<p>ISP + 3h</p></button>
-      <button id=pc3 class="validated">Engrais de lune<p>ISP + 3h</p></button>
-      <button id=pc4 class="validable">Serre photovoltaïque<p>ISP + 3h</p></button>
-      <button id=pc5 class="inaccessible">Serre à gaz solaire<p>ISP + 3h</p></button>
-      <button id=pd1 class="validated">Fillon de thurgonium<p>ISP + 3h</p></button>
-      <button id=pd2 class="validable">Gisement vertical<p>ISP + 3h</p></button>
-      <button id=pd3 class="inaccessible">Mine avec le petit chariot motorisé<p>ISP + 3h</p></button>
-      <button id=pd4 class="inaccessible">Marteau piqueur d'extraction laser<p>ISP + 3h</p></button>
-      <button id=pd5 class="inaccessible">Carrière stellaire<p>ISP + 3h</p></button>
+      <button id=CP0 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP0'))" @click="showChallengeDescription('CP0')">Usine</button>
+      <button id=CP1  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP1'))" @click="showChallengeDescription('CP1')">Source de H2O<p>ISP + 3h</p></button>
+      <button id=CP5  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP5'))" @click="showChallengeDescription('CP5')">Bisse sans fin<p>ISP + 3h</p></button>
+      <button id=CP9  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP9'))" @click="showChallengeDescription('CP9')">Puit stellaire<p>ISP + 3h</p></button>
+      <button id=CP13 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP13'))" @click="showChallengeDescription('CP13')">Pompe à énéergie solaire<p>ISP + 3h</p></button>
+      <button id=CP17 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP17'))" @click="showChallengeDescription('CP17')">Barrage de l'Hongrin<p>ISP + 3h</p></button>
+      <button id=CP2  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP2'))" @click="showChallengeDescription('CP2')">Pile nucléaire<p>ISP + 3h</p></button>
+      <button id=CP6  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP6'))" @click="showChallengeDescription('CP6')">Batterie lunaire<p>ISP + 3h</p></button>
+      <button id=CP10 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP10'))" @click="showChallengeDescription('CP10')">Batterie solaire<p>ISP + 3h</p></button>
+      <button id=CP14 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP14'))" @click="showChallengeDescription('CP14')">Réservoir électrique<p>ISP + 3h</p></button>
+      <button id=CP18 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP18'))" @click="showChallengeDescription('CP18')">Réservoir sans fond<p>ISP + 3h</p></button>
+      <button id=CP3  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP3'))" @click="showChallengeDescription('CP3')">Charrue à air comprimé<p>ISP + 3h</p></button>
+      <button id=CP7  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP7'))" @click="showChallengeDescription('CP7')">Terreau stellaire<p>ISP + 3h</p></button>
+      <button id=CP11 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP11'))" @click="showChallengeDescription('CP11')">Engrais de lune<p>ISP + 3h</p></button>
+      <button id=CP15 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP15'))" @click="showChallengeDescription('CP15')">Serre photovoltaïque<p>ISP + 3h</p></button>
+      <button id=CP19 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP19'))" @click="showChallengeDescription('CP19')">Serre à gaz solaire<p>ISP + 3h</p></button>
+      <button id=CP4  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP4'))" @click="showChallengeDescription('CP4')">Fillon de thurgonium<p>ISP + 3h</p></button>
+      <button id=CP8  v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP8'))" @click="showChallengeDescription('CP8')">Gisement vertical<p>ISP + 3h</p></button>
+      <button id=CP12 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP12'))" @click="showChallengeDescription('CP12')">Mine avec le petit chariot motorisé<p>ISP + 3h</p></button>
+      <button id=CP16 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP16'))" @click="showChallengeDescription('CP16')">Marteau piqueur d'extraction laser<p>ISP + 3h</p></button>
+      <button id=CP20 v-bind:class="getClassFromChallengeStatus(productionChallenges.find(x => x.id === 'CP20'))" @click="showChallengeDescription('CP20')">Carrière stellaire<p>ISP + 3h</p></button>
     </section>
   </section>
 </template>
@@ -295,32 +295,32 @@ export default {
       grid-template-rows: 40px repeat(5,1fr);
     }
 
-    #m0,#e0{
+    #CM0,#CE0{
       grid-column: 2/4;
       grid-row: 6/7;
     }
 
-    #ma1,#ma2,#ma3,#ma4,#ea1,#ea2,#ea3,#ea4{
+    #CM1,#CM3,#CM5,#CM7,#CE1,#CE3,#CE5,#CE7{
       grid-column: 2/3;
     }
 
-    #mb1,#mb2,#mb3,#mb4,#eb1,#eb2,#eb3,#eb4{
+    #CM2,#CM4,#CM6,#CM8,#CE2,#CE4,#CE6,#CE8{
       grid-column: 3/4;
     }
 
-    #ma1,#mb1,#ea1,#eb1{
+    #CM1,#CM2,#CE1,#CE2{
       grid-row: 5/6;
     }
 
-    #ma2,#mb2,#ea2,#eb2{
+    #CM3,#CM4,#CE3,#CE4{
       grid-row: 4/5;
     }
 
-    #ma3,#mb3,#ea3,#eb3{
+    #CM5,#CM6,#CE5,#CE6{
       grid-row: 3/4;
     }
 
-    #ma4,#mb4,#ea4,#eb4{
+    #CM7,#CM8,#CE7,#CE8{
       grid-row: 2/3;
     }
 
@@ -331,44 +331,44 @@ export default {
       grid-template-rows: 40px repeat(6,1fr);
     }
 
-    #p0{
+    #CP0{
       grid-column: 2/7;
       grid-row: 7/8;
     }
 
-    #pa1,#pa2,#pa3,#pa4,#pa5{
+    #CP1,#CP5,#CP9,#CP13,#CP17{
       grid-column: 2/3;
     }
 
-    #pb1,#pb2,#pb3,#pb4,#pb5{
+    #CP2,#CP6,#CP10,#CP14,#CP18{
       grid-column: 3/4;
     }
 
-    #pc1,#pc2,#pc3,#pc4,#pc5{
+    #CP3,#CP7,#CP11,#CP15,#CP19{
       grid-column: 4/5;
     }
 
-    #pd1,#pd2,#pd3,#pd4,#pd5{
+    #CP4,#CP8,#CP12,#CP16,#CP20{
       grid-column: 5/6;
     }
 
-    #pa1,#pb1,#pc1,#pd1{
+    #CP1,#CP2,#CP3,#CP4{
       grid-row: 6/7;
     }
 
-    #pa2,#pb2,#pc2,#pd2{
+    #CP5,#CP6,#CP7,#CP8{
       grid-row: 5/6;
     }
 
-    #pa3,#pb3,#pc3,#pd3{
+    #CP9,#CP10,#CP11,#CP12{
       grid-row: 4/5;
     }
 
-    #pa4,#pb4,#pc4,#pd4{
+    #CP13,#CP14,#CP15,#CP16{
       grid-row: 3/4;
     }
 
-    #pa5,#pb5,#pc5,#pd5{
+    #CP17,#CP18,#CP19,#CP20{
       grid-row: 2/3;
     }
 
